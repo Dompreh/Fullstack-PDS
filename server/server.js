d48a1b56a0480da2228ff56827ba1659a1943df1
Dompreh <stephenasiedu3654@gmail.com>
2024-06-29 11:53:18 +0000

    first commit

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const app = express()
+const authRouter = require('./routes/auth')
+
+//MIDDLEWARES
+app.use(cors())
+app.use(express.json())
+
+//DB Connection
+const DB = process.env.DB
+mongoose.connect(DB).then(()=>{
+    console.log("Database connected successfully")
+}).catch((err) =>{
+    console.log("Error:", err)
+})
+
+//ROUTES
+app.use("/auth", authRouter)
+
+
+//SERVER
+const PORT = process.env.PORT || 7000 
+app.listen(PORT, () =>{
+    console.log(`Server is running on PORT ${PORT}`)
+})
