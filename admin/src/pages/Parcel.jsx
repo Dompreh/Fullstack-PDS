474cec7da8b315524ad07a223ca5efe85f13bd94
Dompreh <stephenasiedu3654@gmail.com>
2024-07-28 19:59:40 +0000

    adding admin basic

---
+++ admin/src/pages/Parcel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Parcel = () => {
+  return (
+    <div className='text-white text-3xl'>Parcel</div>
+  )
+}
+
+export default Parcel
---
+++ admin/src/pages/Users.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Users = () => {
+  return (
+    <div className='text-white text-3xl'>Users</div>
+  )
+}
+
+export default Users
